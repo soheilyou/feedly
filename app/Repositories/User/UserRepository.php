308fec4bf644697b16d97c9b1a369223bbd6d432
soheilyou <myparstik@gmail.com>
2022-09-01 18:33:04 +0430

    add create user to user repository

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -2,8 +2,29 @@
 
 namespace App\Repositories\User;
 
+use App\Models\User;
 use App\Repositories\Base\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
 
-class UserRepository extends BaseRepository implements User
+class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
+    public function __construct(User $model)
+    {
+        parent::__construct($model);
+    }
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return mixed
+     */
+    public function createUser(string $name, string $email, string $password)
+    {
+        return $this->create([
+            "name" => $name,
+            "email" => $email,
+            "password" => $password,
+        ]);
+    }
 }
---
+++ tests/Unit/Repositories/User/UserRepositoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Tests\Unit\Repositories\User;
+
+
+use App\Models\User;
+use App\Repositories\User\UserRepository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private UserRepository $userRepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->userRepository = resolve(UserRepository::class);
+    }
+
+    public function testCreateUser()
+    {
+        $name = 'testName';
+        $email = 'testEmail';
+        $password = 'testPassword';
+        $user = $this->userRepository->createUser($name, $email, $password);
+        $this->assertEquals($name, $user->name);
+        $this->assertEquals($email, $user->email);
+        $this->assertTrue(Hash::check($password, $user->password));
+        $this->assertTrue($user instanceof User);
+    }
+
+    public function testCreateUserCheckDatabase()
+    {
+        $name = 'testName';
+        $email = 'testEmail';
+        $password = 'testPassword';
+        $user = $this->userRepository->createUser($name, $email, $password);
+        // check database
+        $this->assertDatabaseHas('users', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $user->password
+        ]);
+    }
+}
