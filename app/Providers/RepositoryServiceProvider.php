0333da88e29b079ab025c839dbddb8c5f3c98a43
soheilyou <myparstik@gmail.com>
2022-09-02 21:11:04 +0430

    bind FeedRepositoryInterface to FeedRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Feed\FeedRepository;
+use App\Repositories\Feed\FeedRepositoryInterface;
 use App\Repositories\User\UserRepository;
 use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +18,7 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(FeedRepositoryInterface::class, FeedRepository::class);
     }
 
     /**
